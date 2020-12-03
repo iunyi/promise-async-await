78753b9d554ff22728ac1571a0b6a746c73fe065
Junyi Ji <junyiji@gmail.com>
2020-12-03 16:00:14 +0100

    Fetch data

--- Task 3 - Country Info/task3.js
+++ Task 3 - Country Info/task3.js
@@ -1,2 +1,9 @@
-// Task 3: using https://restcountries.eu/ API,
 // get country where alpha3Code = col
+
+const fetchData = async() => {
+    let response = await fetch('https://restcountries.eu/rest/v2/alpha/col');
+    let country = await response.json();
+    console.log(country);
+};
+
+fetchData();
